e3c2608996782ba494191ba06207b617644976e7
Theophil Dio <dtheo.sangma@gmail.com>
2023-11-05 11:04:50 +0600

    Query for retrieve the top 5 customers who have made the highest total purchase amount.

---
+++ query_4.sql
@@ -0,0 +1,16 @@
+-- Write a SQL query to retrieve the top 5 customers who have made the highest total purchase amount. Display the customer name along with the total purchase amount in descending order of the purchase amount.
+
+SELECT
+    c.name AS customer_name,
+    SUM(oi.quantity * oi.unit_price) AS total_purchase_amount
+FROM
+    Customers c
+LEFT JOIN
+    Orders o ON c.customer_id = o.customer_id
+LEFT JOIN
+    Order_Items oi ON o.order_id = oi.order_id
+GROUP BY
+    c.name
+ORDER BY
+    total_purchase_amount DESC
+LIMIT 5;
